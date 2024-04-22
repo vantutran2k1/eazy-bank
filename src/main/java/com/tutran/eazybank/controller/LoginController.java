21832da4003aafcbd2726420e5cc02feff0d5689
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-04-22 21:55:25 +0700

    Add the registration endpoint

---
+++ src/main/java/com/tutran/eazybank/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.tutran.eazybank.controller;
+
+import com.tutran.eazybank.model.Customer;
+import com.tutran.eazybank.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final CustomerRepository customerRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        ResponseEntity<String> response = null;
+        try {
+            customer.setPwd(passwordEncoder.encode(customer.getPwd()));
+            Customer savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + ex.getMessage());
+        }
+        return response;
+    }
+
+}
